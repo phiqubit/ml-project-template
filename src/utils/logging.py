6914c2dd6697ab569d9669404e7d2c6cd4ee1a64
Phi Qubit <suraj.bhardwaj.1.168@gmail.com>
2025-09-19 10:45:01 +0200

    Update logging.py

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -1,1 +1,25 @@
+"""Project-wide logging utilities."""
+import logging
+
+def get_logger(name: str = __name__) -> logging.Logger:
+    """
+    Create or retrieve a configured logger.
+
+    Parameters
+    ----------
+    name : str
+        Logger name.
+
+    Returns
+    -------
+    logging.Logger
+    """
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        fmt = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(message)s")
+        handler.setFormatter(fmt)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+    return logger
 
